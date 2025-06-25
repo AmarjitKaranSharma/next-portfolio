15af3f34034d42fc082f555963a7f3035dc51c8b
AmarjitKaranSharma <amarjitkaransharma@gmail.com>
2025-06-25 22:33:16 +0530

    added new project

--- public/data/project.ts
+++ public/data/project.ts
@@ -1,103 +1,101 @@
 export const ProjectData = [
-  {
-    id: 1,
-    title: "OJC Linx",
-    description: `This platform was created by a nonprofit organization that runs global donation campaigns. They built it to make it easier for people to support meaningful causes from anywhere in the world.
-
-The organization has a team of Admins who manage the system and keep everything running smoothly, and Organization Members who collect donations and handle donor information securely.
-
-Their focus is on transparency, trust, and impact. Everything they do is aimed at bringing people together to support communities in need and create real, lasting change.`,
-    link: "#",
-    images: [
-      {
-        src: "/images/projects/ojc-1.png",
-        alt: "OJC",
-      },
-    ],
-    color: "#9333EA",
-    technologies: [
-      {
-        name: "Angular",
-        icon: "/images/technologies/react.svg",
-      },
-      {
-        name: "Tailwind CSS",
-        icon: "/images/technologies/tailwindcss.svg",
-      },
-      {
-        name: "Node.js",
-        icon: "/images/technologies/nodejs.svg",
-      },
-      {
-        name: "Express.js",
-        icon: "/images/technologies/nodejs.svg",
-      },
-      {
-        name: "MySQL",
-        icon: "/images/technologies/nodejs.svg",
-      },
-    ],
-    details: {
-      projectType: "Web Application (User and Admin Panel)",
-      duration: "4 months",
-      role: "Frontend Developer",
-      keyFeatures: [
-        "Responsive Design",
-        "User Authentication",
-        "Real-time data updates",
-        "Interactive Dashboard",
-        "Custom Payment Gateway",
-      ],
-    },
-  },
-  {
-    id: 2,
-    title: "Safety Cam Ai",
-    description:
-      "SafetyCamAI is a cutting-edge AI-powered system designed to instantly identify criminal threats through advanced face detection technology. Our innovative platform allows you to access one of the largest criminal databases available, providing peace of mind and enhanced security in real-time.",
-    link: "#",
-    color: "#EC4899",
-    images: [
-      {
-        src: "/images/projects/safetycam-1.png",
-        alt: "Project Two Image",
-      },
-    ],
-    technologies: [
-      {
-        name: "Vue JS",
-        icon: "/images/technologies/react.svg",
-      },
-      {
-        name: "Tailwind CSS",
-        icon: "/images/technologies/tailwindcss.svg",
-      },
-      {
-        name: "Python ML/NLP",
-        icon: "/images/technologies/nodejs.svg",
-      },
-      {
-        name: "Python FastApi",
-        icon: "/images/technologies/nodejs.svg",
-      },
-      {
-        name: "MongoDb",
-        icon: "/images/technologies/nodejs.svg",
-      },
-    ],
-    details: {
-      projectType: "SaaS and Admin Panel",
-      duration: "Work in Progress",
-      role: "Frontend Developer",
-      keyFeatures: [
-        "Responsive Design",
-        "User Authentication",
-        "Identifying People's criminal background",
-        "Processing among millions of criminals",
-        "Providing Services to multi-users to keep them safe",
-      ],
-    },
-  },
+  //   {
+  //     id: 1,
+  //     title: "OJC Linx",
+  //     description: `This platform was created by a nonprofit organization that runs global donation campaigns. They built it to make it easier for people to support meaningful causes from anywhere in the world.
+  // The organization has a team of Admins who manage the system and keep everything running smoothly, and Organization Members who collect donations and handle donor information securely.
+  // Their focus is on transparency, trust, and impact. Everything they do is aimed at bringing people together to support communities in need and create real, lasting change.`,
+  //     link: "#",
+  //     images: [
+  //       {
+  //         src: "/images/projects/ojc-1.png",
+  //         alt: "OJC",
+  //       },
+  //     ],
+  //     color: "#9333EA",
+  //     technologies: [
+  //       {
+  //         name: "Angular",
+  //         icon: "/images/technologies/react.svg",
+  //       },
+  //       {
+  //         name: "Tailwind CSS",
+  //         icon: "/images/technologies/tailwindcss.svg",
+  //       },
+  //       {
+  //         name: "Node.js",
+  //         icon: "/images/technologies/nodejs.svg",
+  //       },
+  //       {
+  //         name: "Express.js",
+  //         icon: "/images/technologies/nodejs.svg",
+  //       },
+  //       {
+  //         name: "MySQL",
+  //         icon: "/images/technologies/nodejs.svg",
+  //       },
+  //     ],
+  //     details: {
+  //       projectType: "Web Application (User and Admin Panel)",
+  //       duration: "4 months",
+  //       role: "Frontend Developer",
+  //       keyFeatures: [
+  //         "Responsive Design",
+  //         "User Authentication",
+  //         "Real-time data updates",
+  //         "Interactive Dashboard",
+  //         "Custom Payment Gateway",
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Safety Cam Ai",
+  //     description:
+  //       "SafetyCamAI is a cutting-edge AI-powered system designed to instantly identify criminal threats through advanced face detection technology. Our innovative platform allows you to access one of the largest criminal databases available, providing peace of mind and enhanced security in real-time.",
+  //     link: "#",
+  //     color: "#EC4899",
+  //     images: [
+  //       {
+  //         src: "/images/projects/safetycam-1.png",
+  //         alt: "Project Two Image",
+  //       },
+  //     ],
+  //     technologies: [
+  //       {
+  //         name: "Vue JS",
+  //         icon: "/images/technologies/react.svg",
+  //       },
+  //       {
+  //         name: "Tailwind CSS",
+  //         icon: "/images/technologies/tailwindcss.svg",
+  //       },
+  //       {
+  //         name: "Python ML/NLP",
+  //         icon: "/images/technologies/nodejs.svg",
+  //       },
+  //       {
+  //         name: "Python FastApi",
+  //         icon: "/images/technologies/nodejs.svg",
+  //       },
+  //       {
+  //         name: "MongoDb",
+  //         icon: "/images/technologies/nodejs.svg",
+  //       },
+  //     ],
+  //     details: {
+  //       projectType: "SaaS and Admin Panel",
+  //       duration: "Work in Progress",
+  //       role: "Frontend Developer",
+  //       keyFeatures: [
+  //         "Responsive Design",
+  //         "User Authentication",
+  //         "Identifying People's criminal background",
+  //         "Processing among millions of criminals",
+  //         "Providing Services to multi-users to keep them safe",
+  //       ],
+  //     },
+  //   },
   // {
   //   id: 3,
   //   title: "Project Three",
@@ -125,4 +123,42 @@ Their focus is on transparency, trust, and impact. Everything they do is aimed a
   //     },
   //   ],
   // },
+  {
+    id: 1,
+    title: "Surprise Wink",
+    description:
+      "Surprise Wink is a platform that helps you find the perfect gift for your loved ones. It uses AI to suggest gifts based on the recipient's interests and preferences, making gift-giving easier and more personalized.",
+    link: "https://surprisewink.modernxwebs.com",
+    color: "#EC4899",
+    images: [
+      {
+        src: "/images/projects/surpriseWink-1.png",
+        alt: "Project Four Image 1",
+      },
+      {
+        src: "/images/projects/surpriseWink-2.png",
+        alt: "Project Four Image 2",
+      },
+    ],
+    technologies: [
+      {
+        name: "WordPress",
+        icon: "/images/technologies/react.svg",
+      },
+      {
+        name: "Javascript",
+        icon: "/images/technologies/react.svg",
+      },
+    ],
+    details: {
+      projectType: "Client ",
+      duration: "Work in Progress",
+      role: "Designing and Development",
+      keyFeatures: [
+        "AI-powered gift suggestions",
+        "Personalized gift recommendations",
+        "Responsive Design",
+      ],
+    },
+  },
 ];
